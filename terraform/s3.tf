83f6db0560c8d1f6c24c95d7314b63a59c7f3b8c
Gabriel Soares Maciel <gabriel.maciel@terceirizadoraizen.com>
2024-04-30 10:48:48 -0300

    fix(bucket policy): bucket policy

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -27,7 +27,7 @@ resource "aws_s3_bucket_policy" "boilerplate_angular_aws_s3_cloudfront_bucket_po
         Effect    = "Allow",
         Principal = "*",
         Action    = "s3:GetObject",
-        Resource  = aws_s3_bucket.boilerplate_angular_aws_s3_cloudfront_bucket.arn
+        Resource  = "${aws_s3_bucket.boilerplate_angular_aws_s3_cloudfront_bucket.arn}/*"
       }
     ]
   })
